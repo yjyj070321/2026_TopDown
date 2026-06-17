cfdccd4c1068d88cac03e5217765b8026625a530
yjyj070321 <yjyj070321@gmail.com>
2026-06-17 15:58:31 +0900

    플레이어 피해 효과

--- 2026_TopDown/Assets/Script/PlayerHealth.cs
+++ 2026_TopDown/Assets/Script/PlayerHealth.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     [Header("체력")]
     public int maxHp = 10;
 
+    [Header("피격 효과")]
+    public float hitFlashTime = 0.12f;
+
     private int currentHp;
+
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     void Start()
     {
@@ -16,11 +27,36 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHp -= damage;
 
+        StartCoroutine(DamageFlash());
+
         Debug.Log("플레이어 체력 : " + currentHp);
 
         if (currentHp <= 0)
         {
             Die();
+        }
+    }
+
+    IEnumerator DamageFlash()
+    {
+        if (sr != null)
+        {
+            sr.color =
+                new Color(
+                    1f,
+                    0.4f,
+                    0.4f
+                );
+        }
+
+        yield return new WaitForSeconds(
+            hitFlashTime
+        );
+
+        if (sr != null)
+        {
+            sr.color =
+                Color.white;
         }
     }
 
